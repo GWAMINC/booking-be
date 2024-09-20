53424f11419963486a87416e123df94918bbf863
unknown <eknight1412@gmail.com>
2024-09-20 20:08:51 +0700

    crud api for place type

---
+++ src/main/java/com/GWAMINC/booking_be/repository/PlaceTypeRepository.java
@@ -0,0 +1,7 @@
+package com.GWAMINC.booking_be.repository;
+
+import com.GWAMINC.booking_be.entity.PlaceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceTypeRepository extends JpaRepository<PlaceType, Long> {
+}
